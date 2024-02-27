1bcd2ba658441c8047faefddd236798551c78752
carlos <cbdev2021@gmail.com>
2024-02-27 02:04:05 -0300

    urls user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,8 +11,8 @@ import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', authUser);
+router.post('/', registerUser);
+router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 router
   .route('/profile')
